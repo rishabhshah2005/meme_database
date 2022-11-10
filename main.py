7963582b450d9c11499636659684e6a0e9e9af51
Rishabh <rishabhshah2005@gmail.com>
2022-11-10 23:36:50 +0530

    added tkinter UI

--- main.py
+++ main.py
@@ -3,31 +3,41 @@ import mysql.connector as sql
 con = sql.connect(host='localhost', user='root', passwd='root', database='memes')
 cur = con.cursor()
 
-def highest():
+def highest(cond):
     cur.execute("select * from memes_posted where upvotes = (select max(upvotes) from memes_posted)")
-    ups = cur.fetchall()
+    ups = list(cur.fetchall()[0])
     cur.execute("select * from memes_posted where comments = (select max(comments) from memes_posted)")
-    coms = cur.fetchall()
+    coms = list(cur.fetchall()[0])
     cur.execute("select * from memes_posted where awards = (select max(awards) from memes_posted)")
-    awas = cur.fetchall()
-    print(ups)
-    print(coms)
-    print(awas[0])
+    awas = list(cur.fetchall()[0])
 
-def min():
+    if cond=='a':
+        return awas
+    elif cond=='u':
+        return ups
+    elif cond=='c':
+        return coms
+    
+
+def min(cond):
     cur.execute("select * from memes_posted where upvotes = (select min(upvotes) from memes_posted)")
-    ups = cur.fetchall()
+    ups = list(cur.fetchall()[0])
     cur.execute("select * from memes_posted where comments = (select min(comments) from memes_posted)")
-    coms = cur.fetchall()
+    coms = list(cur.fetchall()[0])
     cur.execute("select * from memes_posted where awards = (select min(awards) from memes_posted)")
-    awas = cur.fetchall()
-    print(ups)
-    print(coms)
-    print(awas[0])
+    awas = list(cur.fetchall()[0])
 
+    if cond=='a':
+        return awas
+    elif cond=='u':
+        return ups
+    elif cond=='c':
+        return coms
+    
 def average(field):
     cur.execute('select distinct template from memes_posted')
     temps = cur.fetchall()
+    ans = []
     max=0
     greatest= ''
     for i in temps:
@@ -36,27 +46,34 @@ def average(field):
         if avg[0][0] > max:
             max = avg[0][0]
             greatest = i[0]
-        print(f"{i[0]} = {avg[0][0]}")
+        ans.append(f"{i[0]} = {avg[0][0]}")
 
-    print(greatest, 'is the most upvoted')
+    ans.append(greatest + ' is the most upvoted')
+    return ans
 
 def most_used():
     cur.execute('select distinct template from memes_posted')
     temps = cur.fetchall()
+    ans = []
     max=0
     greatest= ''
     for i in temps:
         cur.execute(f"select count(template) from (select * from memes_posted where template='{i[0]}')t")
         data = cur.fetchall()
-        print(f"{i} has been used {data[0][0]}")
+        ans.append(f"{i[0]} has been used {data[0][0]} times")
         if data[0][0]>max:
             max=data[0][0]
             greatest = i[0]
-    print(f"{greatest} is used the most")
+    ans.append(f"{greatest} is used the most")
+
+    return ans
 
 def search_id(id):
     cur.execute(f"select * from memes_posted where id={id}")
     result = cur.fetchall()
-    for i in result:
-        print(i)
+    return list(result[0])
 
+if __name__ == '__main__':
+    con = sql.connect(host='localhost', user='root', passwd='root', database='memes')
+    cur = con.cursor()
+    print(most_used())
