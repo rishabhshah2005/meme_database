8494470e2cda2c13260a887847b43e71fefec923
Rishabh <rishabhshah2005@gmail.com>
2022-12-15 15:17:15 +0530

    Added insert delete and update

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
+from tkinter import *
 import mysql.connector as sql
 
-con = sql.connect(host='localhost', user='root', passwd='root', database='memes')
+cols = ['ID', "Template", 'Type', 'Upvotes', 'Comments', 'Awards']
+
+#CONNECTING TO SQL
+con = sql.connect(host='localhost', user='root',
+                  passwd='root', database='memes')
 cur = con.cursor()
 
-def highest(cond):
-    cur.execute("select * from memes_posted where upvotes = (select max(upvotes) from memes_posted)")
-    ups = list(cur.fetchall()[0])
-    cur.execute("select * from memes_posted where comments = (select max(comments) from memes_posted)")
-    coms = list(cur.fetchall()[0])
-    cur.execute("select * from memes_posted where awards = (select max(awards) from memes_posted)")
-    awas = list(cur.fetchall()[0])
+#CREATING MAIN WINDOW
+root = Tk()
 
-    if cond=='a':
-        return awas
-    elif cond=='u':
-        return ups
-    elif cond=='c':
-        return coms
-    
+##############################################################################################################
 
-def min(cond):
-    cur.execute("select * from memes_posted where upvotes = (select min(upvotes) from memes_posted)")
-    ups = list(cur.fetchall()[0])
-    cur.execute("select * from memes_posted where comments = (select min(comments) from memes_posted)")
-    coms = list(cur.fetchall()[0])
-    cur.execute("select * from memes_posted where awards = (select min(awards) from memes_posted)")
-    awas = list(cur.fetchall()[0])
+     
 
-    if cond=='a':
-        return awas
-    elif cond=='u':
-        return ups
-    elif cond=='c':
-        return coms
-    
+def delete_by_id(id):
+    cur.execute(f'delete from memes_posted where id={id}')
+    con.commit()
+
+def insert_stuff(id, temp, typ, ups, comms, awds):
+    cur.execute(f"insert into memes_posted values({id}, '{temp}', '{typ}', {ups}, {comms}, {awds})")
+    con.commit()
+
 def average(field):
     cur.execute('select distinct template from memes_posted')
     temps = cur.fetchall()
     ans = []
-    max=0
-    greatest= ''
+    max = 0
+    greatest = ''
     for i in temps:
-        cur.execute(f"select avg(t.{field}) from (select * from memes_posted where template='{i[0]}')t")
+        cur.execute(
+            f"select avg(t.{field}) from (select * from memes_posted where template='{i[0]}')t")
         avg = cur.fetchall()
         if avg[0][0] > max:
             max = avg[0][0]
@@ -51,29 +41,195 @@ def average(field):
     ans.append(greatest + ' is the most upvoted')
     return ans
 
-def most_used():
-    cur.execute('select distinct template from memes_posted')
-    temps = cur.fetchall()
-    ans = []
-    max=0
-    greatest= ''
-    for i in temps:
-        cur.execute(f"select count(template) from (select * from memes_posted where template='{i[0]}')t")
-        data = cur.fetchall()
-        ans.append(f"{i[0]} has been used {data[0][0]} times")
-        if data[0][0]>max:
-            max=data[0][0]
-            greatest = i[0]
-    ans.append(f"{greatest} is used the most")
+def change(id, temp, typ, ups, comms, awds):
+    cur.execute(f"update memes_posted set template='{temp}', type='{typ}', upvotes={ups}, comments={comms}, awards={awds} where id={id}")
+    con.commit()
 
-    return ans
+
+###############################################################################################################################
+
+# SEARCH
+
 
 def search_id(id):
     cur.execute(f"select * from memes_posted where id={id}")
     result = cur.fetchall()
     return list(result[0])
 
-if __name__ == '__main__':
-    con = sql.connect(host='localhost', user='root', passwd='root', database='memes')
-    cur = con.cursor()
-    print(most_used())
+# CLEAR FRAME
+
+
+def clear_frame(frame):
+    for widgets in frame.winfo_children():
+        widgets.destroy()
+
+# SEARCH BY ID
+
+
+def search_(win):
+    def display(id, frm):
+        clear_frame(frm)
+        data = search_id(id)
+        for i in cols:
+            Label(frm, text=f'{i} = {data[cols.index(i)]}', font=('none', 12)).grid(
+                column=0, rows=cols.index(i)+1, sticky='wn')
+
+    clear_frame(win)
+    frm = Frame(win)
+    frm.grid(row=2, column=0, columnspan=3)
+    Button(win, text="Home", padx=20, pady=15, font=('none', 20, 'bold'),
+           command=lambda: home(root)).grid(row=3, column=0, columnspan=3, pady=40)
+    Label(win, text="Enter ID: ", font=('none', 20),
+          padx=5).grid(row=0, column=0, pady=30)
+    id_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    id_.grid(row=0, column=1)
+    sea = Button(win, justify=LEFT, font=('none', 15), text="SEARCH",
+                 command=lambda: display(id_.get(), frm)).grid(row=0, column=2, padx=5)
+
+
+def delete(win):
+    def display(id, frm):
+        clear_frame(frm)
+        try:
+            delete_by_id(id)
+
+            Label(frm, text='Done', font=('none', 14, 'bold'), padx=5, pady=5).pack()
+
+        except:
+            Label(frm, text='ERROR', font=('none', 14, 'bold'), padx=5, pady=5).pack()
+
+        
+
+    clear_frame(win)
+    frm = Frame(win)
+    frm.grid(row=2, column=0, columnspan=3)
+    Button(win, text="Home", padx=20, pady=15, font=('none', 20, 'bold'),
+           command=lambda: home(root)).grid(row=3, column=0, columnspan=3, pady=40)
+    Label(win, text="Enter ID: ", font=('none', 20),
+          padx=5).grid(row=0, column=0, pady=30)
+    id_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    id_.grid(row=0, column=1)
+    sea = Button(win, justify=LEFT, font=('none', 15), text="Delete",
+                 command=lambda: display(id_.get(), frm)).grid(row=0, column=2, padx=5)
+
+
+def update(win):
+    def display(frm, id, temp, typ, ups, comms, awds):
+        clear_frame(frm)
+
+        change(id, temp, typ, ups, comms, awds)
+        Label(frm, text=f"ID-{id} UPDATED", font=('none', 19, 'bold'), padx=7, pady=7).pack()
+
+    clear_frame(win)
+    frm = Frame(win)
+    frm.grid(row=7, column=0, columnspan=3)
+    Label(win, text="ID", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=0, column=0, pady=10)
+    Label(win, text="Template", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=1, column=0, pady=10)
+    Label(win, text="Type", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=2, column=0, pady=10)
+    Label(win, text="Upvotes", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=3, column=0, pady=10)
+    Label(win, text="Comments", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=4, column=0, pady=10)
+    Label(win, text="Awards", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=5, column=0, pady=10)
+
+    id_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    id_.grid(column=1, row=0, pady=10)
+    temp_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    temp_.grid(column=1, row=1, pady=10)
+    typ_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    typ_.grid(column=1, row=2, pady=10)
+    ups_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    ups_.grid(column=1, row=3, pady=10)
+    comms_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    comms_.grid(column=1, row=4, pady=10)
+    awds_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    awds_.grid(column=1, row=5, pady=10)
+
+
+    Button(win, text="Update", font=('none', 20, 'bold'), padx=7, pady=10, command=lambda: display(frm, id_.get(), temp_.get(), typ_.get(), ups_.get(), comms_.get(), awds_.get())).grid(row=0, column=2, rowspan=3, padx=40)
+    Button(win, text="HOME", font=('none', 20, 'bold'), padx=7, pady=10, command=lambda: home(root)).grid(row=3, column=2, rowspan=3, padx=40)
+
+
+def insert(win):
+    def display(frm, id, temp, typ, ups, comms, awds):
+        clear_frame(frm)
+        try:
+            insert_stuff(id, temp, typ, ups, comms, awds)
+            Label(frm, text=f"ID-{id} INSERTED", font=('none', 19, 'bold'), padx=7, pady=7).pack()
+        except:
+            Label(frm, text="ERROR", font=('none', 19, 'bold'), padx=7, pady=7).pack()
+
+    clear_frame(win)
+    frm = Frame(win)
+    frm.grid(row=7, column=0, columnspan=3)
+    Label(win, text="ID", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=0, column=0, pady=10)
+    Label(win, text="Template", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=1, column=0, pady=10)
+    Label(win, text="Type", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=2, column=0, pady=10)
+    Label(win, text="Upvotes", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=3, column=0, pady=10)
+    Label(win, text="Comments", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=4, column=0, pady=10)
+    Label(win, text="Awards", padx=8, pady=5, font=('none', 15, 'bold')).grid(row=5, column=0, pady=10)
+
+    id_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    id_.grid(column=1, row=0, pady=10)
+    temp_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    temp_.grid(column=1, row=1, pady=10)
+    typ_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    typ_.grid(column=1, row=2, pady=10)
+    ups_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    ups_.grid(column=1, row=3, pady=10)
+    comms_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    comms_.grid(column=1, row=4, pady=10)
+    awds_ = Entry(win, width=17, justify=RIGHT, borderwidth=5, font=('none', 20))
+    awds_.grid(column=1, row=5, pady=10)
+
+
+    Button(win, text="Insert", font=('none', 20, 'bold'), padx=7, pady=10, command=lambda: display(frm, id_.get(), temp_.get(), typ_.get(), ups_.get(), comms_.get(), awds_.get())).grid(row=0, column=2, rowspan=3, padx=40)
+    Button(win, text="HOME", font=('none', 20, 'bold'), padx=7, pady=10, command=lambda: home(root)).grid(row=3, column=2, rowspan=3, padx=40)
+
+
+def avg(win):
+    def display(frm, cond):
+        clear_frame(frm)
+        data = average(cond)
+        for i in data:
+            Label(frm, text=i, font=('none', 12)).grid(
+                column=0, rows=data.index(i)+1, sticky='wn')
+
+    clear_frame(win)
+    win.geometry('600x700')
+    frm = Frame(win)
+    frm.grid(row=2, column=0, columnspan=3, pady=10)
+
+    Label(root, text="Search average of templates in...", font=(
+        'none', 28, 'bold')).grid(row=0, column=0, columnspan=3, pady=10)
+    Button(root, text='Upvotes', padx=30, pady=20, font=('none', 19),
+           command=lambda: display(frm, 'upvotes')).grid(row=1, column=0, padx=8)
+    Button(root, text='Comments', padx=30, pady=20, font=('none', 19),
+           command=lambda: display(frm, 'comments')).grid(row=1, column=1, padx=8)
+    Button(root, text='Awards', padx=30, pady=20, font=('none', 19),
+           command=lambda: display(frm, 'awards')).grid(row=1, column=2, padx=8)
+    Button(win, text="Home", font=("none", 20, 'bold'),
+           command=lambda: home(root)).grid(row=3, column=0, columnspan=3)
+
+# Home function
+
+def home(root):
+    root.geometry("600x510")
+    clear_frame(root)
+    intro = Label(root, text="Meme Database Management", padx=20, pady=20, font=(
+        'none', 30, 'bold')).grid(row=0, column=0, columnspan=2)
+    delete_ = Button(root, text='Delete by ID', padx=20, pady='20', font=(
+        'none', 15, 'bold'), command=lambda: delete(root)).grid(row=1, column=0, pady=20)
+    update_meme = Button(root, text='Update Data', padx=20, pady='20', font=(
+        'none', 15, 'bold'), command=lambda: update(root)).grid(row=1, column=1, pady=20)
+    average = Button(root, text='Average', padx=20, pady='20', font=(
+        'none', 15, 'bold'), command=lambda: avg(root)).grid(row=2, column=0, pady=20)
+    insert_ = Button(root, text='Insert Data', padx=20, pady='20', font=(
+        'none', 15, 'bold'), command=lambda: insert(root)).grid(row=2, column=1, pady=20)
+    search = Button(root, text='Search by ID', padx=60, pady='20', font=(
+        'none', 15, 'bold'), command=lambda: search_(root)).grid(row=3, column=0, columnspan=2, pady=20)
+    Label(root, text="Made by Rishabh Shah XII-A", pady=15, font=('none',
+          15, 'bold')).grid(row=4, column=0, columnspan=2, pady=10)
+
+
+home(root)
+
+root.mainloop()
